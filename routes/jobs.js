159389fa1539511d2777541884585b45b90f1c3f
stifamajstor <stjepan.crncic@gmail.com>
2020-02-10 18:07:26 +0100

    added get req for single job

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const Job = require("../models/Job");
-let data;
 
 router.get("/search", (req, res, next) => {
   axios
@@ -10,9 +9,10 @@ router.get("/search", (req, res, next) => {
       `https://api.adzuna.com/v1/api/jobs/${req.query.location}/search/1?app_id=${process.env.ADZUNA_API_ID}&app_key=${process.env.ADZUNA_API_KEY}&results_per_page=20&what=${req.query.jobTitle}`
     )
     .then(response => {
-      data = response;
-      // res.send(response.data.results);
-      res.render("all-jobs.hbs", { allJobs: response.data.results });
+      res.render("all-jobs.hbs", {
+        allJobs: response.data.results,
+        location: req.query.location
+      });
     })
     .catch(err => {
       console.log(err);
@@ -20,8 +20,17 @@ router.get("/search", (req, res, next) => {
 });
 
 router.get("/:id", (req, res, next) => {
-  console.log("request.params: ", req.params.id);
-  res.send(data.data.results);
+  axios
+    .get(
+      `https://api.adzuna.com/v1/api/jobs/${req.query.location}/search/1?app_id=${process.env.ADZUNA_API_ID}&app_key=${process.env.ADZUNA_API_KEY}&results_per_page=20&what=${req.params.id}`
+    )
+    .then(response => {
+      res.send(response.data.results);
+    });
+  // console.log("request.params: ", req.params.id);
+  // console.log("request.query: ", req.query);
+
+  // res.send(data.data.results);
 
   // const jobId = request.params.id;
 
