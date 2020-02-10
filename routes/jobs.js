7d0266797b491c963becdbc503a03a1864e91cd2
stifamajstor <stjepan.crncic@gmail.com>
2020-02-10 12:43:25 +0100

    fixed the /jobs route

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const Job = require("../models/Job");
 
-router.get("/jobs", (req, res) => {
-  res.render("rooms/all-jobs.hbs");
+router.get("/", (req, res) => {
+  res.render("all-jobs.hbs");
 });
 //
 
