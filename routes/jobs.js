e82604da38d73b91a81f5127886f018df8f037a1
stifamajstor <stjepan.crncic@gmail.com>
2020-02-10 14:20:00 +0100

    displayng jobs from the API

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 const Job = require("../models/Job");
 
-router.get("/", (req, res) => {
-  res.render("all-jobs.hbs");
+router.get("/search", (req, res, next) => {
+  axios
+    .get(
+      `https://api.adzuna.com/v1/api/jobs/${req.query.location}/search/1?app_id=${process.env.ADZUNA_API_ID}&app_key=${process.env.ADZUNA_API_KEY}&results_per_page=50&what=${req.query.jobTitle}`
+    )
+    .then(response => {
+      res.render("all-jobs.hbs", { allJobs: response.data.results });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
-//testing the pulling and pusshing
+
+// router.get("/", (req, res) => {
+//   res.render("all-jobs.hbs");
+// });
 
 module.exports = router;
